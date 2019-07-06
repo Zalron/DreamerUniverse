9b4761c3d672255db059a4d75a85466cf0114e57
Zalron <Davidrlawson@optusnet.com.au>
2019-07-06 16:17:03 +1000

    Added a player script that can move the camera forward

---
+++ Dreamer Universe/Assets/Galaxy Module/Scripts/Player.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float flySpeed = 3f;
+    private Transform cam;
+    private Vector3 velocity;
+    private float horizontal;
+    private float vertical;
+    private float mouseHorizontal;
+    private float mouseVertical;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = GameObject.Find("Main Camera").transform;
+    }
+    public void FixedUpdate()
+    {
+        transform.Rotate(Vector3.up * mouseHorizontal);
+        cam.Rotate(Vector3.right * -mouseVertical);
+        transform.Translate(velocity, Space.World);
+        velocity = ((transform.forward * vertical) + (transform.right * horizontal)) * Time.fixedDeltaTime * flySpeed;
+    }
+        // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+        mouseHorizontal = Input.GetAxis("Mouse X");
+        mouseVertical = Input.GetAxis("Mouse Y");
+        if ()
+        {
+
+        }
+    }
+}

f2ad31aa9dfeaaf76d5a02ac3a4dce05c6185f51
Zalron <Davidrlawson@optusnet.com.au>
2019-08-07 20:27:52 +1000

    Added skill points and skill nodes

--- Dreamer Universe/Assets/Character Module/Scripts/Characters.cs
+++ Dreamer Universe/Assets/Character Module/Scripts/Characters.cs
@@ -43,6 +43,96 @@ namespace CharacterModule
         Level_8,
         Level_9,
         Level_10,
+        Level_11,
+        Level_12,
+        Level_13,
+        Level_14,
+        Level_15,
+        Level_16,
+        Level_17,
+        Level_18,
+        Level_19,
+        Level_20,
+        Level_21,
+        Level_22,
+        Level_23,
+        Level_24,
+        Level_25,
+        Level_26,
+        Level_27,
+        Level_28,
+        Level_29,
+        Level_30,
+        Level_31,
+        Level_32,
+        Level_33,
+        Level_34,
+        Level_35,
+        Level_36,
+        Level_37,
+        Level_38,
+        Level_39,
+        Level_40,
+        Level_41,
+        Level_42,
+        Level_43,
+        Level_44,
+        Level_45,
+        Level_46,
+        Level_47,
+        Level_48,
+        Level_49,
+        Level_50,
+        Level_51,
+        Level_52,
+        Level_53,
+        Level_54,
+        Level_55,
+        Level_56,
+        Level_57,
+        Level_58,
+        Level_59,
+        Level_60,
+        Level_61,
+        Level_62,
+        Level_63,
+        Level_64,
+        Level_65,
+        Level_66,
+        Level_67,
+        Level_68,
+        Level_69,
+        Level_70,
+        Level_71,
+        Level_72,
+        Level_73,
+        Level_74,
+        Level_75,
+        Level_76,
+        Level_77,
+        Level_78,
+        Level_79,
+        Level_80,
+        Level_81,
+        Level_82,
+        Level_83,
+        Level_84,
+        Level_85,
+        Level_86,
+        Level_87,
+        Level_88,
+        Level_89,
+        Level_90,
+        Level_91,
+        Level_92,
+        Level_93,
+        Level_94,
+        Level_95,
+        Level_96,
+        Level_97,
+        Level_98,
+        Level_99,
+        Level_100,
     }
     public class Characters
     {
@@ -50,13 +140,14 @@ namespace CharacterModule
         public CharacterLevel characterLevel;
         public CharacterType characterType;
         public CharacterSpecialisation characterSpecialisation;
+        public int skillpoints;
+        public int spentSkillPoints;
         public int life;
         public int energy;
         public int movementSpeed;
         public int magic;
         public int shield;
         public int armour;
-        
 
         //Stats Extra
         //Life
@@ -96,6 +187,7 @@ namespace CharacterModule
         public int totalBaseArmour;
         public float totalIncreasedArmour;
         public int totalArmour;
+
         //Resistances
         public int magicResistances;
 
@@ -114,5 +206,10 @@ namespace CharacterModule
         public int luckRequired;
         public int intelligenceRequired;
         public int willpowerRequired;
+        public Characters(CharacterLevel _characterLevel, CharacterType _characterType)
+        {
+            characterType = _characterType;
+            characterLevel = _characterLevel;
+        }
     }
 }
---
+++ Dreamer Universe/Assets/Character Module/Scripts/Skillnodes.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace CharacterModule
+{
+    public enum Skillnodetype
+    {
+        Normal,
+        Special,
+    }
+    [CreateAssetMenu(fileName = "Skillnode", menuName = "Character/Skillnode", order = 1)]
+    public class Skillnodes : ScriptableObject
+    {
+        public Skillnodetype skillnodeType;
+        public int lifeGained;
+        public int energyGained;
+        public int movementSpeedGained;
+        public int magicGained;
+        public int shieldGained;
+        public int armourGained;
+
+    }
+}
